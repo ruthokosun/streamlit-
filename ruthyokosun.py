2d2654012225cb321465b2dddbd53577cd9a6373
ruthokosun <130533595+ruthokosun@users.noreply.github.com>
2024-01-29 23:54:12 -0800

    Update ruthyokosun.py

--- ruthyokosun.py
+++ ruthyokosun.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+#import matplotlib.pyplot as plt
 #import seaborn as sns
 #sns.set_style('whitegrid')
 plt.style.use("fivethirtyeight")
